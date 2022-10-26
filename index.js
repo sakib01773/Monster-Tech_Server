9cf0a0720ef257af5745ca62eca4d29fcc3cba0f
sakib01773 <sakibshohan1@gmail.com>
2022-10-26 06:00:29 +0600

    course data added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+
+const courses = require('./courses.json');
 
 app.get('/', (req, res) => {
   res.send('Monster Tech Courses Is Running')
+})
+
+app.get('/course', (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
